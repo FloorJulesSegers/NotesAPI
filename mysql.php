504c6e6b55b103dc17766d63a709e99f2444268f
Floor Jules Segers <floorsegers70@gmail.com>
2019-08-12 15:31:01 +0200

    add note list to php

--- mysql.php
+++ mysql.php
@@ -8,20 +8,13 @@ $myTitle = $_GET['title'];
 $titleClean = filter_var( $myTitle,FILTER_SANITIZE_STRING);
 //echo "$titleClean";
 
-$json = "{
-    'title': '$titleClean',
-    'content': '$contentClean',
-    'made_at': '2019-08-12'
-}";
-
-$jsonData =json_encode($json);
-
-
-
 
 $myContent = $_POST['content'];
 $contentClean = filter_var($myContent, FILTER_SANITIZE_STRING);
 //echo "$contentClean";
+
+$date = date('Y-m-d H:i:s');
+date_default_timezone_set("Europe/Brussels");
 
 
 // Create connection
@@ -30,12 +23,23 @@ $conn = new mysqli($servername, $username, $password,$mydb);
 
 //Select table
 $mysql= "INSERT INTO notes (title, content, made_at)
-            VALUES ('$titleClean', '$contentClean','2019-08-12')";
-$result = $conn->query($jsonData);
+            VALUES ('$titleClean', '$contentClean', '$date')";
+$result = $conn->query($mysql);
 if ($result) {
-    echo "$json";
+    echo ($mysql);
 } else {
     echo "Error: " . $mysql . "<br>" . $conn->error;
+}
+
+$list = "SELECT * FROM notes";
+$resultList = $conn -> query ($list);
+if ($resultList->num_rows > 0) {
+    // output data of each row
+    while($row = $resultList->fetch_assoc()) {
+        echo "<br> Title: ". $row["title"]. " - Content: ". $row["content"]. " " . $row["made_at"] . "<br>";
+    }
+} else {
+    echo "0 results";
 }
 
 $conn->close;
